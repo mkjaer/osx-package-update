21830e73b24d83c5929322c78f2b94e91af314ce
MLikael Kjær <mikael@tlt.local>
2016-03-10 13:50:57 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# OSX Package Update
+
+This will update packages from custom package managers and clean up afterwards.
+
+## Installation
+
+Clone this project and source `osx-package-update.sh` from your profile file.
+
+For bash (from the directory with the code): `echo "source $(pwd)/osx-package-update.sh" >> ~/.bash_profile`
+
+## Configuration
+
+Configuration variables are in the top of the script.
+
+If you need to set a proxy server it can be done with `PROXY_SERVER`
+
+If you're installing brew casks in a non-default folder it can be set with `CASK_DIR`
+
+## Package support
+
+Currently supported packages:
+
+* brew
+* brew cask
+* RubyGems (gem)
+* Atom (apm)
+* Python (pip)
---
+++ osx-package-update.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PROXY_SERVER=""
+CASK_DIR="/opt/homebrew-cask/Caskroom"
+
+function update() {
+ if [[ ! -z "${PROXY_SERVER}" ]]; then
+  local http_proxy="${PROXY_SERVER}"
+  local https_proxy="${PROXY_SERVER}"
+ fi
+ if [[ ! -z "$(which brew)" ]]; then
+  brew update
+  brew upgrade
+  brew cleanup
+  brew prune
+ fi
+ if [[ -d "${CASK_DIR}" ]]; then
+  brew cask install $(brew cask list) 2>/dev/null
+  brew cask cleanup
+  # Delete old versions of cask
+  for folder in $(ls -d "${CASK_DIR}"/*/); do
+   cd $folder
+   if [[ $(ls | wc -l) -gt 1  ]]; then
+     echo $folder is greater than 1
+     echo "Deleting: " $(ls | sort -r | tail -n +2)
+     rm -r $(ls | sort -r | tail -n +2)
+   fi
+  done
+ fi
+ if [[ ! -z "$(which apm)" ]]; then
+  apm update --no-confirm
+  apm upgrade --no-confirm
+ fi
+ if [[ ! -z "$(which pip)" ]]; then
+  pip install --upgrade $(pip list --outdated | cut -f 1 -d " ") 2>/dev/null
+ fi
+ if [[ ! -z "$(which gem)" ]]; then
+  gem update $(gem list --local | cut -f 1 -d " ")
+ fi
+}
